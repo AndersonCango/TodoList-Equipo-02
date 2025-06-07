76665650cf8a83e15fcd21007baec8e4ab631a1d
AndersonCango <118143904+AndersonCango@users.noreply.github.com>
2025-06-07 05:17:44 -0500

    feat: Add profile page

---
+++ src/main/java/madstodolist/controller/PerfilController.java
@@ -0,0 +1,37 @@
+package madstodolist.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import madstodolist.authentication.ManagerUserSession;
+import madstodolist.controller.exception.UsuarioNoLogeadoException;
+import madstodolist.dto.UsuarioData;
+import madstodolist.service.UsuarioService;
+
+@Controller
+public class PerfilController {
+
+     @Autowired
+    UsuarioService usuarioService;
+
+    @Autowired
+    ManagerUserSession managerUserSession;
+
+    private void comprobarUsuarioLogeado(Long idUsuario) {
+        Long idUsuarioLogeado = managerUserSession.usuarioLogeado();
+        if (!idUsuario.equals(idUsuarioLogeado))
+            throw new UsuarioNoLogeadoException();
+    }
+
+    @GetMapping("/perfil")
+    public String verPerfil(Model model) {
+        Long idUsuario = managerUserSession.usuarioLogeado();
+        comprobarUsuarioLogeado(idUsuario);
+
+        UsuarioData usuario = usuarioService.findById(idUsuario);
+        model.addAttribute("usuario", usuario);
+        return "perfil";
+    }
+}
